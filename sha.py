286a035b5f870c63353cd3bdf6d8062cf81660ca
Zhdanov <dzhdanov@scnsoft.com@dzhdanov-mbl.SCNSOFT.COM>
2022-05-14 14:25:41 +0300

    Delete unused files

--- sha.py
+++ sha.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-from functools import partial
-
 import hashlib
 import multiprocessing
 import os
 import sys
+
+from functools import partial
 
 import click
 
@@ -15,46 +15,34 @@ logger = logging.getLogger()
 
 
 class HashFiles:
-    def __init__(self, root_path, algorithm, processes):
-        self.result = ""
+    def __init__(self, root_path, check, algorithm, processes=1):
+        self.check = check
         self.root_path = root_path
         self.algorithm = algorithm
-        if not algorithm:
-            self.algorithm = "sha256"
-        self.processes = processes if processes else 1
+        self.processes = processes
+        self.result = ""
 
-        if root_path:
-            if os.path.isdir(root_path):
-                self.hash_dir(root_path)
-            elif os.path.isfile(root_path):
-                self.hash_file()
+        if not processes:
+            self.processes = 1
+        if self.root_path:
+            self.hash_multiprocessing(self.root_path)
 
     def get_hash_algorithm(self, file):
-        with open(file, "rb") as hash:
-            memory = hashlib.new(self.algorithm)
-            while True:
-                data = hash.read()
-                if not data:
-                    break
-                memory.update(data)
+        # print(
+        #     'Counting hash for file: ' + file + f' with process {multiprocessing.current_process().name} on'
+        #                                             f' {time.ctime()}')
+
+        with open(file, "rb") as hash_file:
+            mem = hashlib.new(self.algorithm)
+            contents = hash_file.read()
+            mem.update(contents)
 
         if "shake" in self.algorithm:
-            return f"{memory.hexdigest(255)}  {file}"
+            return f"{mem.hexdigest(255)}  {file}"
         else:
-            return f"{memory.hexdigest()}  {file}"
+            return f"{mem.hexdigest()}  {file}"
 
-    def hash_multiprocessing(self, file: str):
-        with multiprocessing.Pool(
-                multiprocessing.cpu_count() * self.processes
-        ) as process:
-            process.apply_async(self.get_hash_algorithm, (file,), callback=self.save)
-            process.close()
-            process.join()
-
-    def hash_file(self):
-        self.hash_multiprocessing(self.root_path)
-
-    def hash_dir(self, dirname):
+    def find_files(self):
         files_list = []
         # If argument is file then add it to the list
         if os.path.isfile(self.root_path):
@@ -65,13 +53,43 @@ class HashFiles:
                 filepath = os.path.join(root, name)
                 if os.path.exists(filepath):
                     files_list.append(filepath)
+        return files_list
 
-        for file in files_list:
-            hash_file = HashFiles(file, self.algorithm, self.processes)
-            self.result += str(hash_file) + "\n"
+    def hash_multiprocessing(self, file: str, check=""):
+        with multiprocessing.Pool(
+            multiprocessing.cpu_count() * self.processes
+        ) as process:
+            process.map_async(
+                self.get_hash_algorithm,
+                self.find_files(),
+                callback=partial(self.save, check=self.check),
+            )
+            process.close()
+            process.join()
 
-    def save(self, response):
-        self.result += response
+    def hash_file(self):
+        self.hash_multiprocessing(self.root_path)
+
+    def save(self, response, check, force=False):
+        if not check:
+            for line in response:
+                self.result += line + "\n"
+            for line in response:
+                logger.info(line)
+            # If -c filename is given and got response
+            # If function is called with filepath argument and got response from multiprocessing
+        elif check and not os.path.isdir(check) and response:
+            # Write to file if file doesn't exist or function called with force parameter set to True
+            if not os.path.exists(check) or force:
+                with open(check, "w") as file:
+                    for line in response:
+                        file.write(line + "\n")
+                        self.result += line + "\n"
+            else:
+                logger.info("File already exists")
+                rewrite = input("Do you want to rewrite it? ")
+                if rewrite == "yes" or rewrite == "y":
+                    self.save(response, check, force=True)
 
     def check_file(self, file=""):
         unmatched = []
@@ -103,12 +121,16 @@ class HashFiles:
     "--check",
     "-c",
     help="Read SHA sums from the FILEs and check them or write to file if file (or folder) "
-         "argument is given",
+    "argument is given",
 )
 @click.option("--algorithm", "-a", help="Choose algorithm for hashing")
 @click.option("--processes", "-p", type=int, help="Processes per core")
 @click.option("--algorithms", "-al", is_flag=True, help="Display available algorithms")
 def main(file, check, algorithm, processes, algorithms=True):
+    """
+    Checks if hashes for files changed or not. By default uses sha256 algorithm.
+    """
+
     if algorithms:
         for i in hashlib.algorithms_available:
             logger.info(i)
@@ -117,10 +139,7 @@ def main(file, check, algorithm, processes, algorithms=True):
     if not algorithm:
         algorithm = "sha256"
 
-    if (len(sys.argv) == 1
-        or len(sys.argv) == 3
-        and algorithm in sys.argv
-    ):
+    if len(sys.argv) == 1 or len(sys.argv) == 3 and algorithm in sys.argv:
         for line in sys.stdin:
             b = line.encode()
             m = hashlib.new(algorithm)
@@ -134,15 +153,12 @@ def main(file, check, algorithm, processes, algorithms=True):
         exit(0)
 
     if check and not file:
-        # print(check)
         if os.path.exists(check):
-            check_file = HashFiles(file, algorithm, processes)
+            check_file = HashFiles(file, check, algorithm, processes)
             check_file.check_file(file=check)
         exit(0)
 
-    hashfile = HashFiles(file, algorithm, processes)
-
-    print(hashfile)
+    HashFiles(file, check, algorithm, processes)
 
 
 if __name__ == "__main__":
