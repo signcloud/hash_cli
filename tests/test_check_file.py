dcb33029cb37dafc5939d30d5a7985c008757ad3
Dmitry Zhdanov <signmark@gmail.com>
2022-05-18 17:49:30 +0300

    Update tests

--- tests/test_check_file.py
+++ tests/test_check_file.py
@@ -4,6 +4,6 @@ from sha import HashFiles
 class Test:
     def test_save_file(self):
         check = "hashes"
-        check_file = HashFiles("", check, algorithm='sha256', processes=1)
+        check_file = HashFiles("", check, algorithm="sha256", processes=1)
         result = check_file.check_file(file=check)
         assert isinstance(result, list)
